1a835cceb9782a298713360912f1658c999ee91e
Todd Horst <toddmhorst@yahoo.com>
2014-03-19 18:20:12 -0400

    Create simple.js

---
+++ src/simple.js
@@ -0,0 +1,18 @@
+/*
+Since this is just an example
+Define some bogus functions
+*/
+
+(function ($) {
+
+    $.simpleAdd = function (a,b) {
+		return a+b;
+	};
+	$.simpleSubtract = function (a,b) {
+		return a-b;
+	};
+	$.simpleMultiply = function (a,b) {
+		return a*b;
+	};
+
+})(jQuery);
